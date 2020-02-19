1de681315ba3378b9da8f9f5a0372d08896d7d12
csabourin <christian.sabourin@canada.ca>
2020-02-18 21:09:46 -0500

    Update scrolltest.vue

--- pages/scrolltest.vue
+++ pages/scrolltest.vue
@@ -2,38 +2,36 @@
   <div >
     <ul role="tablist" id="__BVID__29__BV_tab_controls_" ref="choices" :class="[{'isFixed':isScrolled},'nav','nav-tabs','nav-justified']">
       <!---->
-      <li role="presentation" class="nav-item"><a href="#" v-scroll-to="{
+      <li  class="nav-item"><a @click="setColor(1)" href="#" v-scroll-to="{
      el: '#toMee',
      container: 'body',
      duration: 1500,
      easing: 'ease-in-out',
-     offset: -100,
-     force: true,
-     cancelable: true,
+     offset: -200,
      onStart: onStart,
      onDone: onDone,
      onCancel: onCancel,
      x: false,
      y: true
- }"><img data-v-2a183b29="" src="/_nuxt/assets/PrepBudget.jpeg" width="175" height="175"> <br data-v-2a183b29="">
+ }"><img src="/_nuxt/assets/PrepBudget.jpeg" width="175" height="175"> <br >
           Préparer un budget
         </a></li>
-      <li role="presentation" class="nav-item"><a role="tab" aria-selected="true" aria-setsize="5" aria-posinset="2" target="_self" href="#" class="nav-link active" id="__BVID__40___BV_tab_button__" aria-controls="__BVID__40"><img data-v-2a183b29="" src="/_nuxt/assets/Acheter.jpeg" width="175" height="175"> <br data-v-2a183b29="">
+      <li class="nav-item"><a href="javascript:" @click="setColor(2)" class="nav-link active" ><img  src="/_nuxt/assets/Acheter.jpeg" width="175" height="175"> <br >
           Acheter un bien ou un service
         </a></li>
-      <li role="presentation" class="nav-item"><a href="#" class="nav-link"><img data-v-2a183b29="" src="/_nuxt/assets/PayerFacture.jpeg" width="175" height="175"> <br data-v-2a183b29="">
+      <li class="nav-item"><a href="javascript:" class="nav-link active" ><img  src="/_nuxt/assets/PayerFacture.jpeg" width="175" height="175"> <br >
           Payer une facture
         </a></li>
-      <li role="presentation" class="nav-item"><a role="tab" href="#" class="nav-link"><img data-v-2a183b29="" src="/_nuxt/assets/SuivreBudget.jpeg" width="175" height="175"> <br data-v-2a183b29="">
+      <li  class="nav-item"><a role="tab" href="#" class="nav-link active"><img  src="/_nuxt/assets/SuivreBudget.jpeg" width="175" height="175"> <br >
           Suivre un budget
         </a></li>
-      <li role="presentation" class="nav-item"><a role="tab" tabindex="-1" aria-selected="false" aria-setsize="5" aria-posinset="5" target="_self" href="#" class="nav-link" id="__BVID__64___BV_tab_button__" aria-controls="__BVID__64"><img data-v-2a183b29="" src="/_nuxt/assets/FermerAnnee.jpeg" width="175" height="175"> <br data-v-2a183b29="">
+      <li  class="nav-item"><a role="tab" tabindex="-1" aria-selected="false" aria-setsize="5" aria-posinset="5" target="_self" href="#" class="nav-link" ><img  src="/_nuxt/assets/FermerAnnee.jpeg" width="175" height="175"> <br >
           Fermer une année financière
         </a></li>
       <!---->
     </ul>
     <div class="emptyscreen"></div>
-    <div id="toMee" class="showonscreen"></div>
+    <div id="toMee" class="showonscreen" :style="'background-color:'+$parent.$parent.rgbcolor"></div>
     <div class="emptyscreen"></div>
   </div>
 </template>
@@ -51,8 +49,8 @@
 .showonscreen{
 	display: block;
   position: relative;
-  height: 100vh;
-  background-color: #963;
+  height: 90vh;
+  width:100vw;
 }
 
 </style>
@@ -64,6 +62,25 @@ export default {
 		}
 	},
   methods: {
+  	setColor(e){
+  		this.$store.commit('default/setTab',e)
+  	},
+  	toMeeVisible(e){
+  		console.log(e.target.offset().top)
+  		//    var $t            = $(this),
+    //       $w            = $(window),
+    //       viewTop       = document.documentElement.scrollTop,
+    //       viewBottom    = viewTop + $w.height(),
+    //       _top          = $t.offset().top,
+    //       _bottom       = _top + $t.height(),
+    //       compareTop    = partial === true ? _bottom : _top,
+    //       compareBottom = partial === true ? _top : _bottom;
+    
+    // return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+  	},
+  	onCancel(){},
+  	onDone(){},
+  	onStart(){},
     handleScroll(event) {
         if (document.body.scrollTop > 75 || document.documentElement.scrollTop > 75) {
     this.isScrolled=true
